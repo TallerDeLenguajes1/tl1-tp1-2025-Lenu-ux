810c464409378ee1e993819093bf5f0688d792d1
Lenis <lenis99lg@gmail.com>
2026-04-02 19:03:49 -0300

    Ejercicio 4e

--- Punto_2/tp1_1.c
+++ Punto_2/tp1_1.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(){
-    printf("Hola Mundo");
+    printf("Hola Mundo\n");
+    int num=10; //declaro la variable
+    int *p_num=&num; //creo un puntero y le asigno la direccion de la variable
+    printf("1.Contenido del puntero (p_num):%p\n",p_num);
+    printf("2.Direccion de memoria almacenada por el puntero (p_num): %p\n",&p_num);
+    printf("3. Direccion de memoria de la variable (&num):%p\n",&num);
+    printf("4. Direccion de memoria del puntero(&p_num):%p\n",&p_num);
+    printf("5. Tamanio de la variable (sizeof(num)): %lu bytes\n",sizeof(num));
     return 0;
 }
